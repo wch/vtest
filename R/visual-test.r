8424471f78a08f071021c5c6d78cceff8bc97108
Winston Chang <winston@stdout.org>
2012-03-23 01:19:29 -0500

    Add git_check_clean

--- R/visual-test.r
+++ R/visual-test.r
@@ -757,3 +757,19 @@ git_find_commit_hash <- function(dir = ".") {
   else
     stop("Error finding current git commit hash of repo at ", dir, ":", ret$output)
 }
+
+# Check if the state of the git working tree is clean or dirty
+git_check_clean <- function(dir = ".") {
+  ret <- systemCall("git",
+    c("--git-dir", file.path(dir, ".git"), "--work-tree", dir, "diff", "--shortstat"))
+  ret$output <- gsub("\\n$", "", ret$output)  # Remove trailing \n
+
+  if (ret$status == 0) {
+    if (length(ret$output) == 0)
+      return(TRUE)
+    else
+      return(FALSE)
+  } else {
+    stop("Error checking git working tree clean/dity status of ", dir, ":", ret$output)
+  }
+}
