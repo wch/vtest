ca33c43da3e32beb863694f3af6d9e2fd28002d9
Winston Chang <winston@stdout.org>
2012-03-25 14:50:02 -0500

    Big cleanup of vtest

--- R/vtest.r
+++ R/vtest.r
@@ -1,22 +1,23 @@
 # Set the context of the visual tests
-set_vtest_pkg <- NULL
+
+init_vtest <- NULL
 get_vtest_pkg <- NULL
-set_vtest_path <- NULL
-get_vtest_path <- NULL
-set_vtest_resultdir <- NULL
+get_vtest_dir <- NULL
 get_vtest_resultdir <- NULL
-set_vtest_imagedir <- NULL
 get_vtest_imagedir <- NULL
+get_vtest_htmldir <- NULL
+get_vtest_diffdir <- NULL
 
 get_vcontext <- NULL
 set_vcontext <- NULL
-init_vresultset <- NULL
-get_vresultset <- NULL
-append_vresultset <- NULL
+get_vtest_resultset <- NULL
+get_vtest_commits_file <- NULL
+get_vtest_resultsets_file <- NULL
+append_vtest_resultset <- NULL
 
 local({
   pkg <- NULL       # The package object
-  testpath <- NULL  # The path to the test (usually package/visual_test/)
+  testdir <- NULL   # The dir of the test scripts(usually package/visual_test/)
   resultdir <- NULL # Where the database files are saved
   imagedir <- NULL  # Where the image files are saved
 
@@ -25,14 +26,14 @@ local({
   resultset <- NULL  # Information about each test in a context
 
   # These are used by the top-level vtest function
-  set_vtest_pkg <<- function(value) pkg <<- value
   get_vtest_pkg <<- function() pkg
-  set_vtest_path <<- function (value) testpath <<- value
-  get_vtest_path <<- function() testpath
-  set_vtest_resultdir <<- function (value) resultdir <<- value
+  get_vtest_dir <<- function() testdir
   get_vtest_resultdir <<- function() resultdir
-  set_vtest_imagedir <<- function (value) imagedir <<- value
-  get_vtest_imagedir <<- function() imagedir
+  get_vtest_commits_file    <<- function() file.path(resultdir, "commits.csv")
+  get_vtest_resultsets_file <<- function() file.path(resultdir, "resultsets.csv")
+  get_vtest_imagedir <<- function() file.path(resultdir, "images")
+  get_vtest_htmldir  <<- function() file.path(resultdir, "html")
+  get_vtest_diffdir  <<- function() file.path(resultdir, "diff")
 
   # These are used by each test script
   get_vcontext <<- function() context
@@ -41,18 +42,30 @@ local({
     context_count <<- 0
   }
 
-  # Create a zero-row data frame to hold resultset
-  init_vresultset <<- function() {
+  init_vtest <<- function(pkg, testdir = NULL, resultdir = NULL) {
+    parent <- parent.env(environment())  # This is where the variables are
+    parent$pkg <- pkg
+
+    if (is.null(resultdir))  parent$resultdir <- find_default_resultdir(pkg)
+    else                     parent$resultdir <- resultdir
+    
+    if (is.null(testdir))  parent$testdir <- file.path(pkg$path, "visual_test")
+    else                   parent$testdir <- testdir
+
+    # Close context, if open
+    if (!is.null(get_vcontext())) set_vcontext(NULL)
+
+    # Create a zero-row data frame to hold resultset
     cols <- c("context", "desc", "type", "width", "height", "dpi", "err",
               "hash", "order")
     resultset <<- setNames(data.frame(t(rep(NA, length(cols)))), cols)
     resultset <<- resultset[-1, ]
   }
 
-  get_vresultset <<- function() resultset
+  get_vtest_resultset <<- function() resultset
 
   # Add information about a single test
-  append_vresultset <<- function(context, desc, type, width, height, dpi, err, hash, order) {
+  append_vtest_resultset <<- function(context, desc, type, width, height, dpi, err, hash, order) {
     # Check that context + description aren't already used
     if (sum(context == resultset$context & desc == resultset$desc) != 0)
       stop(contest, ":\"", desc, "\" cannot be added to resultset because it is already present.")
@@ -69,54 +82,41 @@ local({
 
 
 # Run visual tests
+# TODO: allow set testdir
 #' @export
 vtest <- function(pkg = NULL, filter = "", resultdir = NULL, showhelp = TRUE) {
   pkg <- as.package(pkg)
   load_all(pkg)
 
-  set_vtest_pkg(pkg)
+  init_vtest(pkg)
 
-  test_path <- file.path(pkg$path, "visual_test")
-  if (!file.exists(test_path)) 
-    return()
-
-  set_vtest_path(test_path)
-
-  if (is.null(resultdir))
-    resultdir <- find_default_resultdir()
-
-  imagedir <- file.path(resultdir, "images")
-
-  set_vtest_resultdir(resultdir)
-  set_vtest_imagedir(imagedir)
+  if (!file.exists(get_vtest_dir()))
+    stop("Visual test script directory does not exist", get_vtest_dir())
 
   if (showhelp)
-    message("Saving test results to directory ", resultdir)
+    message("Saving test results to directory ", get_vtest_resultdir())
 
-  if (!file.exists(resultdir)) {
-    resp <- readline(paste(resultdir, "does not exist! Create? (y/n) "))
+  if (!file.exists(get_vtest_resultdir())) {
+    resp <- readline(paste(get_vtest_resultdir(), "does not exist! Create? (y/n) "))
     if (tolower(resp) != "y")
       return(invisible())
-
-    dir.create(resultdir, recursive = TRUE, showWarnings = FALSE)
-    dir.create(imagedir, recursive = TRUE, showWarnings = FALSE)
-  } else if (!file.exists(imagedir)) {
-    resp <- readline(paste(imagedir, "does not exist! Create? (y/n) "))
-    if (tolower(resp) != "y")
-      return(invisible())
-    dir.create(imagedir, recursive = TRUE, showWarnings = FALSE)
+    dir.create(get_vtest_resultdir(), recursive = TRUE, showWarnings = FALSE)
   }
 
-  if (!is.null(get_vcontext()))  end_vcontext()  # Close context if open
-  init_vresultset()
+  if (!file.exists(get_vtest_imagedir())) {
+    resp <- readline(paste(get_vtest_imagedir(), "does not exist! Create? (y/n) "))
+    if (tolower(resp) != "y")
+      return(invisible())
+    dir.create(get_vtest_imagedir(), recursive = TRUE, showWarnings = FALSE)
+  }
+
 
   # Run the test scripts
-  files <- dir(test_path, full.names = TRUE, include.dirs = FALSE)
+  files <- dir(get_vtest_dir(), full.names = TRUE, include.dirs = FALSE)
   files <- files[grepl("\\.[rR]$", files)]
   files <- match_filter(files, filter)
   files <- files[order(files)]
   lapply(files, source)
-
 
 #  f_quote <- ifelse(is.null(filter), '', paste('filter="', filter, '"', sep = ""))
 #  if (showhelp) {
@@ -133,123 +133,129 @@ vtest <- function(pkg = NULL, filter = "", resultdir = NULL, showhelp = TRUE) {
 
   # If running the full battery of tests, then we can hash the entire test set
   # and compare it to the test set table
-  resultset_hash <- hash_resultset(get_vresultset())
+  resultset_hash <- hash_resultset(get_vtest_resultset())
 
   # Always save results to last_resultset.csv
   message("Saving test results to last_resultset.csv")
-  write.csv(cbind(resultset_hash, get_vresultset()),
-    file.path(resultdir, "last_resultset.csv"), row.names = FALSE)
+  write.csv(cbind(resultset_hash, get_vtest_resultset()),
+    file.path(get_vtest_resultdir(), "last_resultset.csv"), row.names = FALSE)
 
-  # TODO: turn this into function
-  # ============ Check hash of testset results ===========
 
-  commit <- git_find_commit_hash(pkg$path)
-  clean_repo <- git_check_clean(pkg$path)
-
-  # Assume that we'll write the commit data; if certain things happen, set to FALSE
-  write_commitdata <- TRUE
-  # Assume that we'll write the resultset data; if certain things happen, set to FALSE
-  write_resultset <- TRUE
-
-  if (filter != "") {
-    write_commitdata <- FALSE
-    write_resultset <- FALSE
+  if (filter == "")
+    save_last_resultset()
+  else
     message("Did not run the entire set of tests, so the results can't be added to the database.")
-  }
 
-  message("Hash for vtest results is ", resultset_hash)
-  message(pkg$package, " is at commit ", commit)
-  if (clean_repo) {
-    message("Working tree state is clean, so results can be added to database.")
-  } else {
+    save_last_resultset()
+
+}
+
+
+save_last_resultset <- function(prompt = TRUE) {
+
+  # ============ Add to commit tatble ===========
+
+  resultset_hash <- hash_resultset(get_vtest_resultset())
+  message("Hash for resultset is ", resultset_hash)
+
+  # Find current commit of package
+  commit <- git_find_commit_hash(get_vtest_pkg()$path)
+  message("Package ", get_vtest_pkg()$package, " is at commit ", substr(commit, 1, 8))
+
+  # Check for clean working tree
+  if (!git_check_clean(get_vtest_pkg()$path)) {
     message("Working tree state is dirty, so results cannot be added to database.")
-    write_commitdata <- FALSE
-    write_resultset   <- FALSE
+    return(invisible())
   }
+  message("Working tree state is clean, so results can be added to database.")
 
-  # Read existing commit test results
-  if (file.exists(file.path(resultdir, "commits.csv")))
-    commitdata <- read.csv(file.path(resultdir, "commits.csv"))
+  # Read existing commit table
+  if (file.exists(get_vtest_commits_file()))
+    commitdata <- read.csv(get_vtest_commits_file(), stringsAsFactors = FALSE)
   else
     commitdata <- data.frame()
 
-  commitmatch <- commitdata$commit == commit
-  if (any(commitmatch)) {
-    message("Previous results for commit ", substr(commit, 1, 6), " found: ",
-      paste(commitdata$resultset_hash[commitmatch], collapse = ", "))
+  commitmatch <- commitdata[commitdata$commit == commit, ]
+  if (nrow(commitmatch) > 1) {
+    stop("More than one matching commit in commit table. This indicates a problem with the database.")
 
-    if (sum(commitmatch) > 1)
-      stop("More than one matching commit in database. This indicates a problem with the database.")
+  } else if (nrow(commitmatch) == 1) {
+    message("Found existing test results for commit ", substr(commit, 1, 8), ": ",
+      paste(commitmatch$resultset_hash, collapse = ", "))
 
-    if (commitdata$resultset_hash == resultset_hash) {
-      message("Old and current results match! Good.")
-      write_commitdata <- FALSE
+    if (commitmatch$resultset_hash == resultset_hash) {
+      message("For this commit, old and current results match! Good.")
+      return(invisible())
     } else {
-      message("Old and current results do not match! This may be because of changes to R, or to other packages.")
-      if (write_commitdata) {
-        reply <- readline("Replace old test result data with new test result data? (y/n) ")
+      message("For this commit, old and current results do not match! This may be because of changes to R, or to other packages.")
+      if (prompt) {
+        reply <- readline("  Replace old results with new results in database? (y/n) ")
         if (tolower(reply) != "y")
-          write_commitdata <- FALSE
-        else {
-          commitdata <- commitdata[-commitmatch, ]
-          commitdata <- rbind(commitdata, data.frame(commit = commit,
-                                                     resultset_hash = resultset_hash))
-        }
+          return(invisible())
       }
+
+      commitdata <- rbind(commitdata[commitdata$commit != commit, ],
+                      data.frame(commit = commit, resultset_hash = resultset_hash))
     }
   } else {
-    commitdata <- rbind(commitdata, data.frame(commit = commit,
-                                               resultset_hash = resultset_hash))
-
-    reply <- readline("Results are new. Would you like to add them to the database? (y/n) ")
-    if (tolower(reply) != "y") {
-      write_commitdata <- FALSE
-      write_resultset <- FALSE
+    if (prompt) {
+      reply <- readline("Results are new. Would you like to add them to the commit table? (y/n) ")
+      if (tolower(reply) != "y")
+        return(invisible())
     }
+
+    commitdata <- rbind(commitdata,
+                    data.frame(commit = commit, resultset_hash = resultset_hash))
   }
 
-  if (write_commitdata) {
-    message("Writing result hash to commit database.")
-    write.csv(commitdata, file.path(resultdir, "commits.csv"), row.names = FALSE)
-  }
-
-  # TODO: turn this into function
   # ============== Add to the resultset table ======================
 
+  # Assume that we'll write the resultset data; if certain things happen, set to FALSE
+  write_resultset <- TRUE
+
   # Read existing test results
-  if (file.exists(file.path(resultdir, "resultsets.csv")))
-    resultsets <- read.csv(file.path(resultdir, "resultsets.csv"), stringsAsFactors = FALSE)
+  if (file.exists(get_vtest_resultsets_file()))
+    resultsets <- read.csv(get_vtest_resultsets_file(), stringsAsFactors = FALSE)
   else
     resultsets <- data.frame(resultset_hash = character())
+
+  message("Checking if this resultset is already in resultsets table...")
 
   # Get the old results that match the current resultset hash (if present)
   # It would be nice to be able to use:
   #   subset(resultsets, resultset_hash == resultset_hash, select = -resultset_hash)
-  # but this case is very problematic for subset because of re-used var name and
+  # but this case is problematic for subset because of re-used var name and
   # because when there are no matches, subset returns a 1-row NA-filled data frame.
   resultset_match <- resultsets[resultsets$resultset_hash == resultset_hash, , drop = FALSE]
   resultset_match <- resultset_match[!(names(resultset_match) %in% "resultset_hash")]
 
   if (nrow(resultset_match) > 0) {
-    message("Existing results found for resultset hash ", resultset_hash)
-    message("Checking existing result hash just to make sure... ", appendLF = FALSE)
+    message("Found existing resultset with matching hash: ", resultset_hash)
+    message("Recalculating existing resultset hash just to make sure... ", appendLF = FALSE)
     resultset_match_hash <- hash_resultset(resultset_match)
     if (resultset_match_hash != resultset_hash)
-      stop("Re-hashing old resultset results in a different hash value: ",
+      stop("Re-hashing old resultset in a different hash value: ",
            resultset_match_hash,
-           "\nThis indicates a problem with the resultset database.")
+           "\nThis indicates a problem with the resultsets database.")
 
-    message("Hash matches!")
-    message("No need to add new resultset to database.")
+    message("Hash matches! No need to add this resultset to database.")
+
   } else {
-    message("No existing results found for resultset hash ", resultset_hash)
-    if (write_resultset) {
-      message("Adding new resultset to database.")
-
-      resultsets <- rbind(resultsets, cbind(resultset_hash, get_vresultset()))
-      write.csv(resultsets, file.path(resultdir, "resultsets.csv"), row.names = FALSE)
+    message("Did not find existing resultset with matching hash: ", resultset_hash)
+    if (prompt) {
+      reply <- readline("  Add this resultset to the resultsets table? (y/n) ")
+      if (tolower(reply) != "y")
+        return(invisible())
     }
+    resultsets <- rbind(resultsets, cbind(resultset_hash, get_vtest_resultset()))
   }
+
+  # If we made it this far, write to the commit and resultsets tables
+  message("Adding new commit information to commit table.")
+  write.csv(commitdata, get_vtest_commits_file(), row.names = FALSE)
+
+  message("Adding new resultset to resultsets table.")
+  write.csv(resultsets, get_vtest_resultsets_file(), row.names = FALSE)
 }
 
 
@@ -277,7 +283,7 @@ end_vcontext <- function() {
 }
 
 
-# Save an individual test to file, and record information using append_vresultset
+# Save an individual test to file, and record information using append_vtest_resultset
 # This presently only works with pdf; other file types will fail
 # * desc: a short description of the test
 # * filename: output filename (not including extension, like ".pdf"). If NULL, use MD5
@@ -317,7 +323,7 @@ save_vtest <- function(desc = NULL, width = 4, height = 4, dpi = 72, device = "p
     file.rename(cleanpdf, file.path(get_vtest_imagedir(), filehash))
 
   # Append the info for this test in the vis_info list
-  append_vresultset(context = get_vcontext(), desc = desc,
+  append_vtest_resultset(context = get_vcontext(), desc = desc,
     type = device, width = width, height = height, dpi = dpi,
     err = err, hash = filehash)
 
