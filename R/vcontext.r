3cbb335f5ca15ffe4805949913f1161ceff75dfe
Winston Chang <winston@stdout.org>
2012-04-02 16:00:28 -0500

    Clean up vcontext local environment

--- R/vcontext.r
+++ R/vcontext.r
@@ -1,24 +1,19 @@
-get_vcontext <- NULL
-set_vcontext <- NULL
-get_vcontext_count <- NULL
-inc_vcontext_count <- NULL
 
+# An environment to hold information about a context
+vc <- new.env(hash = TRUE, parent = emptyenv())
 
-local({
-  context <- NULL       # The context of a set of tests (usually in one script)
-  context_count <- NULL # Keep count of tests, within this context
+vc$context <- NULL       # The context of a set of tests (usually in one script)
+vc$context_count <- NULL # Keep count of tests, within this context
 
-  # These are used by each test script
-  get_vcontext <<- function() context
-  set_vcontext <<- function(value) {
-    context <<- value
-    context_count <<- 0
-  }
+# These are used by each test script
+get_vcontext <- function() vc$context
+set_vcontext <- function(value) {
+  vc$context <- value
+  vc$context_count <- 0
+}
 
-  get_vcontext_count <<- function() context_count
-  inc_vcontext_count <<- function(value) context_count <<- context_count + 1
-
-})
+get_vcontext_count <- function() vc$context_count
+inc_vcontext_count <- function(value) vc$context_count <- vc$context_count + 1
 
 
 #' Start a visual test context
