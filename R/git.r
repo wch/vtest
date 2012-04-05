a1aaa88fd0a32e0d787d87899a07e2a7cd5d9313
Winston Chang <winston@stdout.org>
2012-04-04 22:30:54 -0500

    git_prev_commits: add ability to get all commits, and entire history

--- R/git.r
+++ R/git.r
@@ -31,15 +31,21 @@ git_check_clean <- function(dir = ".") {
 
 #' Return the last n commit hashes of a git repository
 #'
-#' @param dir  directory containing the git repository
-#' @param n  number of commit hashes to return
-#' @param start  the commit to search backward from
+#' @param dir  directory containing the git repository.
+#' @param n  number of commit hashes to return. If \code{NULL} return all commits.
+#' @param start  the commit to search backward from.
+#' @param all  if \code{TRUE} return all commits including ones not on current
+#'   branch (like \code{git log --all}).
 #'
 #' @return a character vector of commit hashes, or an empty vector if there
 #'   was a problem executing the git command.
-git_prev_commits <- function(dir = ".", n = 20, start = "") {
-  ret <- systemCall("git", c("--git-dir", file.path(dir, ".git"), "log",
-    "--format='%H'", str_c("-", n), start))
+git_prev_commits <- function(dir = ".", n = 20, start = "", all = FALSE) {
+  args <- c("--git-dir", file.path(dir, ".git"), "log", "--format='%H'")
+
+  if (!is.null(n))  args <- c(args, str_c("-", n))
+  if (all)          args <- c(args, "--all")
+
+  ret <- systemCall("git", c(args, start))
 
   if (ret$status == 0) {
     return(strsplit(ret$output, "\n")[[1]])
